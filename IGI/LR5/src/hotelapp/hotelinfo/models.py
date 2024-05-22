db5a4e90bffec578f418fd282de94c27c78375d0
georga399 <georga399@gmail.com>
2024-05-22 22:48:38 +0300

    Start django project. Adding models

---
+++ IGI/LR5/src/hotelapp/hotelinfo/models.py
@@ -0,0 +1,30 @@
+from django.db import models
+from django.contrib.auth import get_user_model
+
+# Create your models here.
+class Article(models.Model):
+    title = models.CharField(max_length=50)
+    body = models.CharField(max_length=2000)
+    last_modified_date = models.DateTimeField(auto_now=True) 
+
+class FAQ(models.Model):
+    question = models.CharField(max_length=200)
+    answer = models.CharField(max_length=200)
+
+class Discount(models.Model):
+    title = models.CharField(max_length=50)
+    description = models.CharField(max_length=200)
+    start_date = models.DateTimeField(auto_now=False, auto_now_add=False)
+    expire_date = models.DateTimeField(auto_now=False, auto_now_add=False)
+
+class Job(models.Model):
+    position = models.CharField(max_length=50)
+    description = models.CharField(max_length=500)
+
+class EmployeeProfile(models.Model):
+    position = models.CharField(max_length=50)
+    user = models.OneToOneField(get_user_model(), on_delete = models.CASCADE, primary_key = True)
+
+class Review(models.Model):
+    body = models.CharField(max_length=500)
+    user = models.OneToOneField(get_user_model(), on_delete = models.CASCADE, primary_key = True)
---
+++ IGI/LR5/src/hotelapp/hotelsystem/models.py
@@ -0,0 +1,32 @@
+from django.db import models
+from django.contrib.auth import get_user_model
+# Create your models here.
+class RoomCategory(models.Model):
+    name = models.CharField(max_length=20)
+
+class RoomDescription(models.Model):
+    description = models.CharField(max_length=1000)
+
+class Room(models.Model):
+    number = models.CharField(max_length=5)
+    category = models.ForeignKey(RoomCategory, null=True, on_delete=models.SET_NULL)
+    description = models.ForeignKey(RoomDescription, null=True, on_delete=models.SET_NULL)
+
+#TODO: set default name of file
+# class Image(models.Model):
+#     room_description = models.ForeignKey(RoomDescription, on_delete=models.SET_NULL)
+#     image = models.ImageField(upload_to='media/')
+    
+class Reservation(models.Model):
+    user = models.ForeignKey(get_user_model(), null=True, on_delete=models.SET_NULL)
+    adults_count = models.IntegerField()
+    register_datetime = models.DateTimeField()
+    start_date = models.DateTimeField()
+    end_date = models.DateTimeField()
+    price = models.DecimalField(max_digits = 5, decimal_places=2)
+
+class Payment(models.Model):
+    user = models.ForeignKey(get_user_model(), null=True, on_delete=models.SET_NULL)
+    description = models.CharField(max_length=200)
+    reservation = models.ForeignKey(Reservation, null=True, on_delete=models.SET_NULL)
+    amount = models.DecimalField(max_digits = 5, decimal_places=2)
