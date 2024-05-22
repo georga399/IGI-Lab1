6e3b1f6573f8181ba9baf4a1b3b4d8d807ecbcca
georga399 <georga399@gmail.com>
2024-05-22 23:41:37 +0300

    Registering models in admin panel

--- IGI/LR5/src/hotelapp/hotelsystem/admin.py
+++ IGI/LR5/src/hotelapp/hotelsystem/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import RoomCategory, RoomDescription, Room, Payment, Reservation
 
 # Register your models here.
+admin.site.register(RoomCategory)
+admin.site.register(RoomDescription)
+admin.site.register(Room)
+admin.site.register(Payment)
+admin.site.register(Reservation)
